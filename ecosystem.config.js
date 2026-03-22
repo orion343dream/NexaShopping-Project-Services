f6cdd7ab9b3a316c7b4568544aa33fa7d586a3c6
Dilusha Sandaruwan <156986226+orion343dream@users.noreply.github.com>
2026-03-22 20:53:55 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,32 +2,32 @@ module.exports = {
   apps : [
     {
       name   : "cloud-sql-auth-proxy",
-      script : "./cloud-sql-proxy silent-bird-489817-g0:asia-southeast1:mysql-vm silent-bird-489817-g0:asia-southeast1:postgres-vm --private-ip",
-      log_file: "./logs/cloud-sql-auth-proxy-out.log",
+      script : "./cloud-sql-proxy",
+      args   : "silent-bird-489817-g0:asia-southeast1:mysql-vm silent-bird-489817-g0:asia-southeast1:postgres-vm --private-ip",
     },
     {
-    name   : "user-service",
-    script : "java -jar ./user-service/target/User-Service-1.0.0.jar",
-    instances : 2,
-    log    : "./logs/user-service.log",
-    error  : "./logs/user-service-error.log"
-  },
-
-  {
-    name   : "item-service",
-    script : "java -jar ./item-service/target/Item-Service-1.0.0.jar",
-    instances : 2,
-    log    : "./logs/item-service.log",
-    error  : "./logs/item-service-error.log"
-  },
-
-  {
-    name   : "order-service",
-    script : "java -jar ./order-service/target/Order-Service-1.0.0.jar",
-    instances : 2,
-    log    : "./logs/order-service.log",
-    error  : "./logs/order-service-error.log"
-  }
-
+      name      : "user-service",
+      script    : "java",
+      args      : "-Xmx256m -jar ./user-service/target/User-Service-1.0.0.jar",
+      instances : 1,
+      log       : "./logs/user-service.log",
+      error     : "./logs/user-service-error.log"
+    },
+    {
+      name      : "item-service",
+      script    : "java",
+      args      : "-Xmx256m -jar ./item-service/target/Item-Service-1.0.0.jar",
+      instances : 1,
+      log       : "./logs/item-service.log",
+      error     : "./logs/item-service-error.log"
+    },
+    {
+      name      : "order-service",
+      script    : "java",
+      args      : "-Xmx256m -jar ./order-service/target/Order-Service-1.0.0.jar",
+      instances : 1,
+      log       : "./logs/order-service.log",
+      error     : "./logs/order-service-error.log"
+    }
   ]
 }
